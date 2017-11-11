70cfc6cae0f78e1f7d2a8b1e0b7e29fa16dd5b58
Bruno Arakaki <brn.arakaki@gmail.com>
2017-11-11 10:47:55 -0200

    makefile and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# projeto-compiladores
+# Projeto de Compiladores
 PCS2056
+
+## Intrucoes para rodar o projeto
+O projeto pode ser executado utilizando o arquivo `Makefile`, atraves do comando:
+```bash
+$ make run
+```
+Esse comando ira compilar o arquivo fonte, rodar o programa executavel e em
+seguida apagar o arquivo executavel gerado.
+
+
+Caso necessite apenas gerar o executavel, isso pode ser obtido simplesmente rodando:
+```bash
+$ make
+```
+
+
+## Autores
+Bruno Arakaki
+
+Mayer Levy
--- main.c
+++ main.c
@@ -14,13 +14,16 @@ char input[] = "for (int a = 0; a < 10; a++) { if (a == 6) return; }";
 
 const char *commands[] = {"if","else","for", "while", "do", "break", "continue", "return", "switch", "case", "default", "int", "char", "float", "double", "void", "struct", "typedef"};
 
-const char *symbols[] = {"==", "<=", ">=", "!=", "<", ">", "=", "{", "}", "(", ")", "[", "]", "&&", "&", "||", "|", "+", "-", "%", "*", "/"};
+const char *symbols[] = {"==", "<=", ">=", "!=", "<", ">", "=", "{", "}", "(", ")", "[", "]", "&&", "&", "||", "|", "+", "-", "\%", "*", "/"};
 
 const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m" , "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
 const char *numbers[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 const char end_chars[] = {' ', '\n', ';'};
+
+
+
 
 int isCommand(char *word);
 int isSymbol(char *word);
