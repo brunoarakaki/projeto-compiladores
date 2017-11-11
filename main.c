f23babfb3c17bbbd5397e6fbc3cc233f2da5d725
Bruno Arakaki <brn.arakaki@gmail.com>
2017-11-11 11:58:54 -0200

    finding some tokens

--- main.c
+++ main.c
@@ -14,48 +14,45 @@ const char *commands[] = {"if","else","for", "while", "do", "break", "continue",
 	"void", "struct", "typedef"};
 
 const char *symbols[] = {"==", "<=", ">=", "!=", "<", ">", "=", "{", "}",
-	"(", ")", "[", "]", "&&", "&", "||", "|", "+", "-", "\%", "*", "/"};
+	"(", ")", "[", "]", "&&", "&", "||", "|", "+", "-", "*", "/"};
 
 const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
 	"l", "m" , "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
 	"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
-const char *numbers[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+const char *digits[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 const char *end_chars[] = {' ', '\n', ';'};
 
 
 int isCommand(char *word);
 int isSymbol(char *word);
+int tokenFound(char *word, char *type);
 
 int main(int argc, const char * argv[]) {
 	char* input_file_name;
-	char input_char;
 	FILE *input_file;
 	char word[256];
 	int w_i = 0;
 	char **tokens;
+	int input_char;
 
 	input_file_name = "input_file";
 	input_file = fopen(input_file_name, "rt");
 
 	// le todo o arquivo de entrada ate encontrar EOF (end of file)
-	while(fscanf(input_file, "%c", &input_char) == 1) {
-
-		if (strchr(end_chars, input_char)) {
-			if (isCommand(word)) {
-				printf("Achou comando: %s\n", word);
-			} else if (isSymbol(word)) {
-				printf("Achou simbolo: %s\n", word);
-			}
-			memset(word,0,strlen(word));
-			w_i = 0;
-			continue;
-		}
+	while ((input_char = fgetc(input_file)) != EOF) {
 		word[w_i] = input_char;
-		printf("%c -- %s\n", input_char, word);
+		//printf("%c --(%d) %s\n", input_char, w_i, word);
 		w_i++;
+		if (strchr(end_chars, input_char)) {
+			w_i = tokenFound(word, "end_char");
+		} else if (isCommand(word)) {
+			w_i = tokenFound(word, "Comando");
+		} else if (isSymbol(word)) {
+			w_i = tokenFound(word, "Simbolo");
+		}
 	}
 
 	fclose(input_file);
@@ -77,5 +74,12 @@ int isSymbol(char *word) {
 			return 1;
 		}
 	}
+	return 0;
+}
+
+
+int tokenFound(char *word, char *type){
+	printf("Achou %s: %s\n", type, word);
+	memset(word,0,strlen(word));
 	return 0;
 }
