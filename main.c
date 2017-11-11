157601614b92d67ac70425aa840ad16bed0641f4
Bruno Arakaki <brn.arakaki@gmail.com>
2017-11-11 11:30:13 -0200

    reading external file

--- main.c
+++ main.c
@@ -5,35 +5,45 @@
 //  Created by Mayer Levy on 10/1/17.
 //  Copyright © 2017 Mayer Levy. All rights reserved.
 //
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-char input[] = "for (int a = 0; a < 10; a++) { if (a == 6) return; }";
+const char *commands[] = {"if","else","for", "while", "do", "break", "continue",
+	"return", "switch", "case", "default", "int", "char", "float", "double",
+	"void", "struct", "typedef"};
 
-const char *commands[] = {"if","else","for", "while", "do", "break", "continue", "return", "switch", "case", "default", "int", "char", "float", "double", "void", "struct", "typedef"};
+const char *symbols[] = {"==", "<=", ">=", "!=", "<", ">", "=", "{", "}",
+	"(", ")", "[", "]", "&&", "&", "||", "|", "+", "-", "\%", "*", "/"};
 
-const char *symbols[] = {"==", "<=", ">=", "!=", "<", ">", "=", "{", "}", "(", ")", "[", "]", "&&", "&", "||", "|", "+", "-", "\%", "*", "/"};
-
-const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m" , "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+	"l", "m" , "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+	"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
 const char *numbers[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-const char end_chars[] = {' ', '\n', ';'};
-
-
+const char *end_chars[] = {' ', '\n', ';'};
 
 
 int isCommand(char *word);
 int isSymbol(char *word);
 
 int main(int argc, const char * argv[]) {
+	char* input_file_name;
+	char input_char;
+	FILE *input_file;
 	char word[256];
 	int w_i = 0;
 	char **tokens;
-	for (int i = 0; i < sizeof(input); i++) {
-		if (strchr(end_chars, input[i])) {
+
+	input_file_name = "input_file";
+	input_file = fopen(input_file_name, "rt");
+
+	// le todo o arquivo de entrada ate encontrar EOF (end of file)
+	while(fscanf(input_file, "%c", &input_char) == 1) {
+
+		if (strchr(end_chars, input_char)) {
 			if (isCommand(word)) {
 				printf("Achou comando: %s\n", word);
 			} else if (isSymbol(word)) {
@@ -43,10 +53,13 @@ int main(int argc, const char * argv[]) {
 			w_i = 0;
 			continue;
 		}
-		word[w_i] = input[i];
+		word[w_i] = input_char;
+		printf("%c -- %s\n", input_char, word);
 		w_i++;
 	}
-    return 0;
+
+	fclose(input_file);
+  return 0;
 }
 
 int isCommand(char *word) {
